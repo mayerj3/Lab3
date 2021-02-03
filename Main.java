8ce86edfd6b05a2af66ba189a8ec95e9b16ba364
mayerj3 <mayerj3@mail.uc.edu>
2021-02-03 04:54:14 +0000

    Added in if and else if to always count to zero from both negative and positive

--- Main.java
+++ Main.java
@@ -2,16 +2,25 @@ import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
 
-    Scanner s = new Scanner(System.in);
-    int num;
-
-    System.out.println("Enter a number to count down or up from: ");
-    num = s.nextLine();
-    doCountDown(num);
-
-    
-
-
-    System.out.println("Blast Off!");
+    int a, b = 0;
+		Scanner s = new Scanner(System.in);		
+		System.out.print("Enter a number to count down or up from:");
+		a = s.nextInt();
+		System.out.print("Enter 0:");
+		b = s.nextInt();
+		
+			if(a<b){					
+				do{
+					System.out.println(a);
+					a++; }
+				while(a<=b);
+				}		
+			else if (b<a){					
+				do{
+					System.out.println(a);
+					a--; }
+				while(b<=a);
+				}
+		System.out.println("Blast off!");
   }
 }
